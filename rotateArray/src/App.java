dcec6afcb29886b0f751ce8cbfe43c47444517e3
Ghirmay <abomordecai@gmail.com>
2022-12-22 20:27:53 -0500

    Changed rotate to void method from int array method.

--- rotateArray/src/App.java
+++ rotateArray/src/App.java
@@ -3,19 +3,18 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         int[] nums = new int[]{1,2,3,4,5,6,7};
-        System.out.println(Arrays.toString(rotateArray(nums, 3)));
+        rotate(nums,3);
+        System.out.println(Arrays.toString(nums));
     }
 
-
-    static int[] rotateArray(int[] input, int rotate){
+    static void rotate(int[] input, int rotate) {
         int len = input.length;
         int[] output = new int[len];
         int realRotation = Math.abs((len-rotate));
         realRotation %= len;
-
         for(int i = 0; i < len; i++){
             output[i] = input[(i+realRotation)%len];
         }
-        return output;
+        System.arraycopy(output, 0, input, 0, input.length);
     }
 }
