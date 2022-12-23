6b1f3cb5667721bb76482f89bb2c7d829c726328
Ghirmay <abomordecai@gmail.com>
2022-12-22 19:54:47 -0500

    First draft. Works up to when rotation factor is equal to length of array

--- rotateArray/src/App.java
+++ rotateArray/src/App.java
@@ -1,5 +1,22 @@
+import java.util.*;
+import java.lang.*;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] nums = new int[]{1,2,3,4,5,6,7};
+        System.out.println(Arrays.toString(rotateArray(nums, 3)));
+    }
+
+
+    static int[] rotateArray(int[] input, int rotate){
+        int len = input.length;
+        int[] output = new int[len];
+        int realRotation = (len-rotate);
+
+        for(int i = 0; i < len; i++){
+            output[i] = input[(i+realRotation)%len];
+        }
+        return output;
     }
 }
